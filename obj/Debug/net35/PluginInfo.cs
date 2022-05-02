b6b7a93f1583eec0b752eba25a57e2428b91ead3
MrGentle <pellejensen94@gmail.com>
2022-05-02 21:26:25 +0200

    uhh more files for glom

---
+++ obj/Debug/net35/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace GentleSwap
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "GentleSwap";
+        public const string PLUGIN_NAME = "GentleSwap";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
